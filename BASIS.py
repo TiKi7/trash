831a7ede19c14191d84b42694820e08d2eb35cba
shiyonsufa <shiyon.sufa@gmail.com>
2018-05-10 22:36:25 +0430

    readability
    update 1.0

--- BASIS.py
+++ BASIS.py
@@ -3,50 +3,50 @@ print('@@@@@@ BASIS CONVERTOR @@@@@@')
 print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ (Basis1 -> Basis2)')
 hexa = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
 hex_a = {'10': 'A', '11': 'B', '12': 'C', '13': 'D', '14': 'E', '15': 'F'}
-stepA = ''
+medium = ''
 
 # input ##########################################
 n = int(input('#Basis 1: '))
 m = int(input('#Basis 2: '))
 if n == 16:
     print('Tip: Your Numbers Should Follow This Pattern\n1, 2, 3, 4, 5, 6, 7, 8, 9, A(10), B(11), C(12), D(13), E(14), F(15).\n')
-N = input('#Enter your number: ')
-N_list = list(N)
+raw = input('#Enter your number: ')
+raw_list = list(raw)
 
 # convert from n -> 10 ###########################
 if n == 10:
-    stepA = int(N)
+    medium = int(raw)
 else:
-    stepA = 0
-    for i in range(len(N_list)):
+    medium = 0
+    for i in range(len(raw_list)):
         temp = 0
         i2 = -i - 1
-        if N_list[i2] in hexa:
-            temp = hexa[N_list[i2]]
+        if raw_list[i2] in hexa:
+            temp = hexa[raw_list[i2]]
         else:
-            temp = int(N_list[i2])
-        stepA = stepA + temp * (n**i)
-print('#Converetd to BASIS(10)= ' + str(stepA))
+            temp = int(raw_list[i2])
+        medium = medium + temp * (n**i)
+print('#Converetd to BASIS(10)= ' + str(medium))
 
 # convert from 10 -> m ###########################
-stepB = ''
+result = ''
 if m == 10:
-    stepB = str(stepA)
+    result = str(medium)
 else:
     while True:
-        tempo = int(stepA / m)
-        temp = str(stepA % m)
+        tempo = int(medium / m)
+        temp = str(medium % m)
         if temp in hex_a:
             temp = hex_a[temp] 
-        stepB = str(temp) + stepB
+        result = str(temp) + result
         if tempo < m:
             if tempo in hex_a:
                 tempo = hex_a[tempo]        
-            stepB = str(tempo) + stepB
+            result = str(tempo) + result
             break
-        stepA = tempo
+        medium = tempo
 
 # print result ###################################
-print('#Converted to BASIS(' + str(m) + ')= ' + str(stepB))
+print('#Converted to BASIS(' + str(m) + ')= ' + str(result))
 print('\n\nTo be or not to be :o')
 input()
